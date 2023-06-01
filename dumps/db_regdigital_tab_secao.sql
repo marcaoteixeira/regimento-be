306a707ab877bea8fd331b5de1143409a3f76611
marcaoteixeira <marcao.teixeira@gmail.com>
2023-06-01 15:27:02 -0300

    Commit 001 01/06

--- dumps/db_regdigital_tab_secao.sql
+++ dumps/db_regdigital_tab_secao.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_regdigital
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,7 +32,7 @@ CREATE TABLE `tab_secao` (
   KEY `FK_tab_secao_tab_titulo` (`id_titulo`),
   CONSTRAINT `FK_tab_secao_tab_titulo` FOREIGN KEY (`id_titulo`) REFERENCES `tab_titulo` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `secao->capitulo` FOREIGN KEY (`id_capitulo`) REFERENCES `tab_capitulo` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Seções do Regimento Interno Facilitado Câmara dos Deputados';
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Seções do Regimento Interno Facilitado Câmara dos Deputados';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-29 12:33:57
+-- Dump completed on 2023-06-01 15:26:29
---
+++ dumps/zeratabela.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `tab_artigo` AUTO_INCREMENT=1
