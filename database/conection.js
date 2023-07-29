8d5c63767d10258b3a609a9f11400e8759ff4efc
marcaoteixeira <marcao.teixeira@gmail.com>
2023-07-29 15:19:24 -0300

    commit 001 29/07/2023

--- database/conection.js
+++ database/conection.js
@@ -1,9 +1,17 @@
 const knex = require('knex')({
+    //client: 'mysql2',
+   // connection: {
+        //host: 'localhost',
+        //port: 3306,
+        //user: 'root',
+        //password: 'dani039808',
+        //database: 'db_regdigital'
+   // }
     client: 'mysql2',
     connection: {
-        host: 'localhost',
+        host: 'regfacilitado-db.ctigr16bsvyp.sa-east-1.rds.amazonaws.com',
         port: 3306,
-        user: 'root',
+        user: 'admin',
         password: 'dani039808',
         database: 'db_regdigital'
     }
